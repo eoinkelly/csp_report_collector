14777ada9ada8376e6d164c75089a16dba57c9e8
Eoin Kelly <eoin@rabidtech.co.nz>
2018-04-29 16:42:31 +1200

    Add very basic display of saved reports

--- lib/csp_report_collector_web/controllers/page_controller.ex
+++ lib/csp_report_collector_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule CspReportCollectorWeb.PageController do
   use CspReportCollectorWeb, :controller
+  require Logger
+  import Ecto.Query
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    query = from(r in CspReportCollector.CspReport)
+    reports = CspReportCollector.Repo.all(query)
+    Logger.info("#{inspect(reports)}")
+
+    render(conn, "index.html", reports: reports)
   end
 end
