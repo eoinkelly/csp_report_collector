a0b9e2ce6b2828b4ad9809101e91db9ceb2b4bf7
Eoin Kelly <eoin@rabidtech.co.nz>
2018-04-29 19:45:08 +1200

    Only show most recent 500 reports on homepage

--- lib/csp_report_collector_web/controllers/page_controller.ex
+++ lib/csp_report_collector_web/controllers/page_controller.ex
@@ -3,7 +3,7 @@ defmodule CspReportCollectorWeb.PageController do
   import Ecto.Query
 
   def index(conn, _params) do
-    query = from(r in CspReportCollector.CspReport, order_by: [desc: :inserted_at])
+    query = from(r in CspReportCollector.CspReport, order_by: [desc: :inserted_at], limit: 500)
     reports = CspReportCollector.Repo.all(query)
     render(conn, "index.html", reports: reports)
   end
