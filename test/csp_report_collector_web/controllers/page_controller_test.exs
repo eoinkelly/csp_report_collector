25e66f0a0e0ab1020dc6869427790cda49e93c5a
Eoin Kelly <eoin@rabidtech.co.nz>
2018-04-30 07:25:06 +1200

    Improve controller test

--- test/csp_report_collector_web/controllers/page_controller_test.exs
+++ test/csp_report_collector_web/controllers/page_controller_test.exs
@@ -1,11 +1,26 @@
 defmodule CspReportCollectorWeb.PageControllerTest do
   use CspReportCollectorWeb.ConnCase
 
-  test "Attempting to GET / requires HTTP Basic Auth", %{conn: conn} do
-    conn = get(conn, "/")
+  @username System.get_env("BASIC_AUTH_USERNAME")
+  @password System.get_env("BASIC_AUTH_PASSWORD")
 
-    assert List.first(Plug.Conn.get_resp_header(conn, "www-authenticate")) =~ ~r{Basic realm}
-    assert conn.status === 401
-    assert conn.resp_body === "401 Unauthorized"
+  test "GET / without basic auth credentials prevents access", %{conn: conn} do
+    conn = conn |> get("/")
+
+    assert response(conn, 401) =~ "401 Unauthorized"
+  end
+
+  test "GET / with basic auth credential shows the homepage", %{conn: conn} do
+    conn =
+      conn
+      |> using_basic_auth(@username, @password)
+      |> get("/")
+
+    assert html_response(conn, 200) =~ "CSP Report"
+  end
+
+  defp using_basic_auth(conn, username, password) do
+    header_content = "Basic " <> Base.encode64("#{username}:#{password}")
+    conn |> put_req_header("authorization", header_content)
   end
 end
